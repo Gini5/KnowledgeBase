81da7f01fc6421cb68a4f2ca9458e5fb74f48e91
Gini <gini5@163.com>
2018-07-03 22:04:09 +0800

    840 optimization

--- 840MagicSquaresInGrid.py
+++ 840MagicSquaresInGrid.py
@@ -1,3 +1,22 @@
+class Solution(object):
+    def numMagicSquaresInside(self, grid):
+        R, C = len(grid), len(grid[0])
+        ans = 0
+        numbers = [x for x in range(1, 10)]
+        
+        def magic(a,b,c,d,e,f,g,h,i):
+            return (sorted([a,b,c,d,e,f,g,h,i]) == numbers and
+                (a+b+c == d+e+f == g+h+i == a+d+g ==
+                 b+e+h == c+f+i == a+e+i == c+e+g == 15))
+
+        for r in range(R-2):
+            for c in range(C-2):
+                if magic(grid[r][c], grid[r][c+1], grid[r][c+2],
+                         grid[r+1][c], grid[r+1][c+1], grid[r+1][c+2],
+                         grid[r+2][c], grid[r+2][c+1], grid[r+2][c+2]): ans += 1
+        return ans
+
+        
 class Solution:
     def numMagicSquaresInside(self, grid):
         """
