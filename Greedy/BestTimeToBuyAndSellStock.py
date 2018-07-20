71ea18f35389ab4af56a669b0ee8c2eca7a542e5
Gini5 <gini5@163.com>
2018-07-20 14:22:05 +0800

    maxprofitwithcooldown

--- Greedy/BestTimeToBuyAndSellStock.py
+++ Greedy/BestTimeToBuyAndSellStock.py
@@ -29,3 +29,23 @@ class Solution(object):
             profit = max(profit, p + buy)
             buy = max(buy, profit - p - fee)
         return profit
+
+    # with 1 cool down day after selling
+    def maxProfitWithCooldown(self, prices):
+        n = len(prices)
+        if n < 2: return 0
+
+        profit = [0 for _ in range(n)]
+        buy = [float('-inf') for _ in range(n)]
+        for i in range(n):
+            if i == 0:
+                buy[i] = -prices[i]  # first day: set buy to first price
+            elif i == 1:
+                profit[i] = max(profit[i - 1], prices[i] + buy[i - 1])  # compare to yesterday, decide sell or not
+                buy[i] = max(buy[i - 1],
+                             profit[i - 1] - prices[i])  # compare to yesterday's profit, decide buy new or not
+            else:
+                profit[i] = max(profit[i - 1], prices[i] + buy[i - 1])  # compare to yesterday, decide sell or not
+                buy[i] = max(buy[i - 1], profit[i - 2] - prices[
+                    i])  # compare to the day before yesterday's profit, decide buy or not
+        return profit[-1]
