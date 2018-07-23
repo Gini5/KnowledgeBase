902785095dca5d38b87d1ecce86333f2dafcae02
Gini5 <gini5@163.com>
2018-07-23 20:59:03 +0800

    linked list has cycle, remove elements and is palidrome

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -26,3 +26,46 @@ def deleteDuplicates(self, head):
         else:
             head = head.next
     return res
+
+def hasCycle(self, head):
+    if not head:
+        return False
+    walker = head
+    runner = head.next
+    try:
+        while walker != runner:
+            walker = walker.next
+            runner = runner.next.next
+        return True
+    except:
+        return False
+
+def removeElements(self, head, val):
+    dummy = ListNode(-1)
+    dummy.next = head
+    cur = dummy
+    while cur:
+        while cur.next and cur.next.val == val:
+            cur.next = cur.next.next
+        cur = cur.next
+    return dummy.next
+
+def isPalindrome(self, head):
+    slow = fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    node = None
+    while slow:
+        nxt = slow.next
+        slow.next = node
+        node = slow
+        slow = nxt
+
+    while node and head:
+        if node.val != head.val:
+            return False
+        node = node.next
+        head = head.next
+    return True
