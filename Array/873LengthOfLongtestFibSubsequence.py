ed3530f9cc590fa2e9e48af02238888de9133608
Gini5 <gini5@163.com>
2018-07-25 17:27:46 +0800

    873

---
+++ Array/873LengthOfLongtestFibSubsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lenLongestFibSubseq(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        n = len(A)
+        s = set(A)
+        res = 2
+        for i in range(n):
+            for j in range(i+1,n):
+                a,b,l = A[i], A[j], 2
+                while a+b in s:
+                    a, b, l = b, a+b, l+1
+                res = max(res,l)
+        return res if res > 2 else 0
