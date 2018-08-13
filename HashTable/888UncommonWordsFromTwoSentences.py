8bb5c52bcb3b467f8872873425706c4d77cf4344
Gini5 <gini5@163.com>
2018-08-13 17:18:27 +0800

    888

---
+++ HashTable/888UncommonWordsFromTwoSentences.py
@@ -0,0 +1,10 @@
+import collections
+class Solution:
+    def uncommonFromSentences(self, A, B):
+        """
+        :type A: str
+        :type B: str
+        :rtype: List[str]
+        """
+        c = collections.Counter((A + " " + B).split())
+        return [w for w in c if c[w] == 1]
