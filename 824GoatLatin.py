e387a7f8840451eabd8893f8eb80ba65696d3fe7
Gini5 <gini5@163.com>
2018-08-17 18:18:58 +0800

    824

--- 824GoatLatin.py
+++ 824GoatLatin.py
@@ -7,10 +7,8 @@ class Solution:
         words = S.split(' ')
         vowel = 'aeiouAEIOU'
         for i,w in enumerate(words):
-            if w and w[0] in vowel:
-                new = w+'ma'
-            elif w and w[0] not in vowel:
-                new = w[1:]+w[0]+'ma'
-            new += 'a'*(i+1)
+            if w and w[0] not in vowel:
+                new = w[1:]+w[0]
+            new += 'ma'+'a'*(i+1)
             words[i] = new
         return ' '.join(words)
