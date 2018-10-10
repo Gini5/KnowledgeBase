f6decf1f79d290249c3c6fb0a04bb6fffa69e082
Gini5 <gini5@163.com>
2018-10-10 18:00:07 +0800

    binary search

--- GeneralMethod/BinarySearch.py
+++ GeneralMethod/BinarySearch.py
@@ -2,16 +2,21 @@ times = [1,4,6,8,10]
 t = 11
 
 # binary search, if not hit, return left element
-l,r = 0, len(times)-1
+l,r = 0, len(times)
 while l<r:
-    mid = (r+l)//2
-    if times[mid] == t:
-        break
-    elif times[mid] < t:
-        l = mid+1
-        mid = l
+    mid = (l+r)//2
+    if times[mid] <= t:
+        l = mid + 1
     else:
-        r = mid-1
-        mid = r
-if mid > 0 and times[mid] > t: mid -= 1
-print(mid)
+        r = mid
+print(l-1)
+
+# if not hit, return right element
+l, r = 0, len(times)
+while l<r:
+    mid = (l+r)//2
+    if times[mid]>=t:
+        r = mid -1
+    else:
+        l = mid+1
+print(r+1)
