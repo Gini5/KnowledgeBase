8e0c695dd5f7a20c6e869e60c3449651c51bd577
Gini5 <gini5@163.com>
2018-10-11 10:32:13 +0800

    binary search

--- GeneralMethod/BinarySearch.py
+++ GeneralMethod/BinarySearch.py
@@ -1,7 +1,8 @@
 times = [1,4,6,8,10]
-t = 11
+t = 3
 
 # binary search, if not hit, return left element
+# means to find the first > t, so if mid<=t, ignore
 l,r = 0, len(times)
 while l<r:
     mid = (l+r)//2
@@ -12,11 +13,13 @@ while l<r:
 print(l-1)
 
 # if not hit, return right element
+# means to find the first < t, so if mid>=t, ignore
+# be careful to set mid as the right element, or in case [1,4] t = 5 will get endless loop
 l, r = 0, len(times)
 while l<r:
-    mid = (l+r)//2
+    mid = (l+r)//2+1
     if times[mid]>=t:
         r = mid -1
     else:
-        l = mid+1
+        l = mid
 print(r+1)
