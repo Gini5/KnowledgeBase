c74139418ee12d4c07e1c974a8cf03991ca53798
Gini5 <gini5@163.com>
2018-11-01 17:51:52 +0800

    931

--- Array/931MinimumFallingPathSum.py
+++ Array/931MinimumFallingPathSum.py
@@ -4,11 +4,6 @@ class Solution:
         :type A: List[List[int]]
         :rtype: int
         """
-        """
-        1 2 3
-        4 5 6
-        7 8 9
-        """
         m = len(A)
         if m == 1: return A[0][0]
         for i in range(1,m):
@@ -20,3 +15,20 @@ class Solution:
                 else:
                     A[i][j] += min(A[i-1][j],A[i-1][j-1])
         return min(A[m-1])
+
+
+class Solution:
+    def minFallingPathSum(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: int
+        """
+        m = len(A)
+
+        for row in range(1, m):
+            A[row][0] += min(A[row - 1][0:2])
+            for col in range(1, m - 1):
+                A[row][col] += min(A[row - 1][col - 1:col + 2])
+            A[row][m - 1] += min(A[row - 1][m - 2:m])
+
+        return min(A[m - 1])
