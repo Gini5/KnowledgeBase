a40515500a31a113814c47cbfd42cb3a1b7890e1
Gini5 <gini5@163.com>
2018-11-13 10:34:44 +0800

    singleton with __new__

--- DesignPattern/Singleton.py
+++ DesignPattern/Singleton.py
@@ -1,26 +1,29 @@
 #The singleton pattern is used to ensure one and only one object from a class gets created.
+# class Singleton(object):
+#     __instance = None
+#
+#     def __init__(self):
+#         if not Singleton.__instance:
+#             print('Instance does not exist')
+#         else:
+#             print('Instance exists:', self.get_instance())
+#
+#     @classmethod
+#     def get_instance(cls):
+#         if not cls.__instance:
+#             cls.__instance = Singleton()
+#         return cls.__instance
+
 class Singleton(object):
-    __instance = None
-
-    def __init__(self):
-        if not Singleton.__instance:
-            print('Instance does not exist')
-        else:
-            print('Instance exists:', self.get_instance())
-
-    @classmethod
-    def get_instance(cls):
-        if not cls.__instance:
-            cls.__instance = Singleton()
-        return cls.__instance
+    _singletons = {}
+    def __new__(cls):
+        if not cls._singletons.keys():            #若还没有任何实例
+            cls._singletons[cls] = object.__new__(cls)  #生成一个实例
+        return cls._singletons[cls]
 
 a = Singleton()
-
-# Once a singleton is created, all instances of this class will use the same object
-print('Creating instance', Singleton.get_instance())
-
-# Since singleton already created, the existing object is returned
+print(id(a))
 b = Singleton()
-
-# Even though we now have 3 different instances of this class, they all reference the same object
+print(id(b))
 c = Singleton()
+print(id(b))
