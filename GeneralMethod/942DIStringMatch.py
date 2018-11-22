b5ee7a6a2637258e0375fe8517fa376149e2360d
Gini5 <gini5@163.com>
2018-11-22 16:35:03 +0800

    924

---
+++ GeneralMethod/942DIStringMatch.py
@@ -0,0 +1,16 @@
+class Solution:
+    def diStringMatch(self, S):
+        """
+        :type S: str
+        :rtype: List[int]
+        """
+        left = right = 0
+        res = [0]
+        for i in S:
+            if i == "I":
+                right += 1
+                res.append(right)
+            else:
+                left -= 1
+                res.append(left)
+        return [i-left for i in res]
