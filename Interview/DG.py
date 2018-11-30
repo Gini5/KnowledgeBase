e840373a3f48ac17f9bbd9913eb6ef8f57124889
Gini5 <gini5@163.com>
2018-11-30 13:58:55 +0800

    add get top 2

--- Interview/DG.py
+++ Interview/DG.py
@@ -7,3 +7,20 @@ def PowerSetsRecursive2(items):
 
 
 print(PowerSetsRecursive2([1,2,3,4]))
+
+
+def top2(l):
+    if len(l)<2: return
+    top1, top2 = l[0], l[1]
+    if top1<top2: top1, top2 = top2, top1
+
+    for item in l[2:]:
+        if item>top1:
+            if top1>top2: top2 = top1
+            top1 = item
+        elif item>top2:
+            top2 = item
+    return [top1,top2]
+
+print(top2([1,3,2,4,7,5]))
+print(top2([7,3,2,4,7,5]))
