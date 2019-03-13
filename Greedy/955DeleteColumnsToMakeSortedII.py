34a408838b072711cfd7fa53a8c0f535cdecfe9d
Gini5 <gini5@163.com>
2019-03-13 17:52:48 +0800

    955

--- Greedy/955DeleteColumnsToMakeSortedII.py
+++ Greedy/955DeleteColumnsToMakeSortedII.py
@@ -1,10 +1,5 @@
 class Solution:
     def minDeletionSize(self, A):
-        if not A: return 0
-        res = 0
-        for j in range(len(A[0])):
-            for i in range(1,len(A)):
-                if A[i-1][j] > A[i][j]:
-                    res += 1
-                    break
-        return res
+        """
+        delete at least how many columns to make each line in lexicographic order
+        """
