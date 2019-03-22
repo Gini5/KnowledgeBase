f1e52e7325fbe08660d9cbeba0efc0d2716ee277
Gini5 <gini5@163.com>
2019-03-22 14:53:53 +0800

    300

--- DP/960DeleteColumnsToMakeSortedIII.py
+++ DP/960DeleteColumnsToMakeSortedIII.py
@@ -4,9 +4,16 @@ class Solution:
         :type A: List[str]
         :rtype: int
         """
-
+        if not A: return 0
+        # dp[i] is till i column, max. increasing subsequence
+        dp = [0 for _ in range(len(A[0]))]
+        for i in range(1,)
 
 t = Solution()
 print(t.minDeletionSize(["babca","bbazb"]))   #3
 print(t.minDeletionSize(["edcba"]))     #4
 print(t.minDeletionSize(["ghi","def","abc"]))   #0
+
+
+#babca
+#bbazb
