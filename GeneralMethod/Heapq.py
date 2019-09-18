2612c6264f90d83ef933d020e734957f2c8d7f0b
yilanhu1 <gini.hu@intel.com>
2019-09-18 14:27:33 +0800

    973 and add heapq

---
+++ GeneralMethod/Heapq.py
@@ -0,0 +1,5 @@
+"""
+heap[k] <= heap[2*k+1] and heap[k] <= heap[2*k+2] for all k, counting elements from zero. 
+For the sake of comparison, non-existing elements are considered to be infinite. 
+The interesting property of a heap is that its smallest element is always the root, heap[0].
+"""
